7404cb4a15589c5afc049fe51809caf688c1ab14
Kuba Krzywanski <kszygy@gmail.com>
2021-05-20 11:54:46 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './assets/styles.scss';
+
+const App = () => (
+  <div className="App">
+    <header style={{ marginTop: '20vh' }}>
+      <h1>Euro 20/21 fixtures</h1>
+    </header>
+  </div>
+);
+
+export default App;
