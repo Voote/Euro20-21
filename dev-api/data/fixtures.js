f48b57a03923e1ac66047d396b9125ed224d0eda
Kuba Krzywanski <kszygy@gmail.com>
2021-05-28 18:31:05 +0100

    first commit

---
+++ dev-api/data/fixtures.js
@@ -0,0 +1,56 @@
+module.exports = [
+  {
+    id: 0,
+    name: 'match1',
+    result: 0,
+    team1: 0,
+    team2: 0,
+    group: 'A',
+    day: 'Friday'
+  },
+  {
+    id: 1,
+    name: 'match2',
+    result: 0,
+    team1: 0,
+    team2: 0,
+    group: 'A',
+    day: 'Friday'
+  },
+  {
+    id: 2,
+    name: 'match2',
+    result: 1,
+    team1: 2,
+    team2: 0,
+    group: 'A',
+    day: 'Friday'
+  },
+  {
+    id: 3,
+    name: 'match3',
+    result: 0,
+    team1: 0,
+    team2: 0,
+    group: 'A',
+    day: 'Friday'
+  },
+  {
+    id: 4,
+    name: 'match4',
+    result: 0,
+    team1: 0,
+    team2: 0,
+    group: 'A',
+    day: 'Friday'
+  },
+  {
+    id: 5,
+    name: 'match5',
+    result: 0,
+    team1: 0,
+    team2: 0,
+    group: 'A',
+    day: 'Friday'
+  }
+];
